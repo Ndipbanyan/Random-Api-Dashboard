9e4f7bbf488253749c925b0b6a0a5ad1d01dff89
Ndipbanyan <agnesjedidiah09@gmail.com>
2021-01-11 11:34:37 +0100

    left component

--- src/components/Components.style.js
+++ src/components/Components.style.js
@@ -9,11 +9,19 @@ export const MainContainer = styled.div`
   opacity: 1;
 `;
 
-export const Container = styled.div`
-  width: 50%;
-  height: 100%;
-  background: yellowgreen;
-  border:1px solid;
+export const Container = styled.div.attrs((props) => ({
+  width: props.width || "50%",
+  height: props.height || "100%",
+  background: props.background || "none",
+  display: props.display ,
+  size:props.size||"none"
+}))`
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  background: ${(props) => props.background};
+  display:${props => props.display};
+  margin-right:${props=>props.size};
+  /* border: 1px solid; */
   opacity: 1;
 `;
 export const RightContainer = styled(Container)`
@@ -27,17 +35,25 @@ export const RightContainer = styled(Container)`
 `;
 
 export const Wrapper = styled.div`
-/* background:blue; */
-width:80%;
-height:90%;
-margin:3rem auto;
-`
-export const Label = styled.label.attrs((props) => ({
+  /* background: blue; */
+  width: 80%;
+  height: 90%;
+  margin: 3rem auto;
+  color: #262A41;
+`;
+export const LeftWrapper = styled(Wrapper)`
+  margin: 12rem 0 0 6rem;
+  height: 50%;
+  color:#fff;
+`;
+export const Label = styled.h1.attrs((props) => ({
   size: props.size || "2rem",
   color: props.color || "#262A41",
+  space: props.space || "0rem"
 }))`
   font-size: ${(props) => props.size};
-  
+  color: ${(props) => props.color};
+  margin-top: ${(props) => props.space};
 `;
 export const Heading = styled.span.attrs((props) => ({
   size: props.size || "2rem",
@@ -46,4 +62,55 @@ export const Heading = styled.span.attrs((props) => ({
 }))`
   font-size: ${(props) => props.size};
   font-weight: ${(props) => props.weight};
+  color: ${(props) => props.color}
 `;
+export const Paragraph = styled.p.attrs((props) => ({
+  size: props.size || "2rem",
+  weight: props.weight || "100",
+  color: props.color || "#262A41",
+}))`
+  color: ${(props) => props.color};
+  font-weight: ${(props) => props.weight};
+  font-size: 0.8rem;
+`;
+
+export const InputFieldWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  padding-left: 1rem;
+  background: #3c3f54;
+  width: ${(props) => (props.primary ? "30rem" : "50px")};
+  height: ${(props) => (props.primary ? "3.5rem" : "50px")};
+  margin-top: ${(props) => (props.primary ? "2rem" : "1rem")};
+  font-size: ${(props) => (props.primary ? "1.2rem" : ".5rem")};
+  border-radius: ${(props) => (props.primary ? "1rem" : "20px")};
+  color: #fff;
+`;
+export const InputField = styled.input`
+  margin-left: 1.4rem;
+  height: ${(props) => (props.primary ? "3rem" : "1rem")};
+  width: ${(props) => (props.primary ? "26rem" : "50px")};
+  font-size: ${(props) => (props.primary ? "1rem" : ".5rem")};
+  background: #3c3f54;
+  outline: none;
+  border: none;
+  color: #fff;
+`;
+export const Buttons = styled.button.attrs((props) => ({
+  size: props.size || "2rem",
+  weight: props.weight || "100",
+  color: props.color || "#262A41",
+  font:props.font||"16px"
+}))`
+width:${(props) => props.size};
+height:${(props) => props.size};
+margin-top:2rem;
+margin-right:3rem;
+background:${(props) => props.color};
+font-size:${props=>props.font};
+border-radius:1rem;
+border:none;
+color:#fff;
+text-align:center;
+`;
+
--- src/components/LeftContainer/index.js
+++ src/components/LeftContainer/index.js
@@ -1,5 +1,9 @@
 import React from 'react'
-import { Container,Label, Heading} from '../Components.style'
+import { 
+    Container, LeftWrapper, Label, Heading, Paragraph,InputFieldWrapper,InputField,Buttons } from '../Components.style'
+import { RiSearchLine } from 'react-icons/ri'
+import { FaUsers, FaMale, FaFemale } from "react-icons/fa";
+
 
 
 
@@ -8,10 +12,55 @@ import { Container,Label, Heading} from '../Components.style'
 export default function LeftContainer() {
     return (
       <Container>
-        <Label size="1.5rem">
-          <Heading >Hello,</Heading>
-          Emerald
-        </Label>
+        <LeftWrapper>
+          <Label size="1.5rem" color="#fff">
+            <Heading color="#fff">Hello,</Heading>
+            Emerald
+          </Label>
+          <Paragraph color="#fff">
+            Welcome to your dashboard, kindly sort through the user base
+          </Paragraph>
+          <InputFieldWrapper primary>
+            <RiSearchLine />
+            <InputField placeholder="Find a user" primary />
+          </InputFieldWrapper>
+          <Label size=".9rem" space="3rem" color="#fff">
+            Show Users
+          </Label>
+          <Container display="flex" width="10rem" height="10rem" >
+            <Container
+              width="6rem"
+              height="4rem"
+              
+            >
+              <Buttons size="4rem" color="#F935A9" font="1.8rem">
+                <FaUsers />
+              </Buttons>
+              <Paragraph color="#fff" weight="400">
+                All Users
+              </Paragraph>
+            </Container>
+            <Container
+              width="6rem"
+              heght="4rem"
+            >
+              <Buttons size="4rem" color="#30BBB5" font="1.8rem">
+                <FaMale />
+              </Buttons>
+              <Paragraph color="#fff" weight="400">
+                Male Users
+              </Paragraph>
+            </Container>
+            <Container width="6rem" height="4rem" >
+              <Buttons size="4rem" color="#7946C1" font="1.8rem">
+                <FaMale />
+              </Buttons>
+              <Paragraph color="#fff" weight="400">
+                Female Users
+              </Paragraph>
+            </Container>
+          </Container>
+        </LeftWrapper>
       </Container>
     );
 }
